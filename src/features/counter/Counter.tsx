09b18a8acd233eeb9b03f89eb763eed3cff14d4f
Nicolas Cena <nicolasnahuelcena@gmail.com>
2022-10-30 21:40:05 +0100

    Init project

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import { useAppSelector, useAppDispatch } from '../../context/hooks';
 import {
   decrement,
   increment,
