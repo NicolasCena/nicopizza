8ff79c78618a4c1dc82ecb8bf6ea2f3b94d15fdc
Nicolas Cena <nicolasnahuelcena@gmail.com>
2022-11-02 22:09:24 +0100

    Pruebas icon font awesome and structure header and home

--- src/router/Navigation.tsx
+++ src/router/Navigation.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
-import { Home } from '../containers/Home';
+import { Home } from '../pages/Home';
 import { Login } from '../components/Login';
 
 export const Navigation = () => {
