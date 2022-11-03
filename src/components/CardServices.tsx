1ddda6f917f06b9e04d9c5217b399661c0455791
Nicolas Cena <nicolasnahuelcena@gmail.com>
2022-11-03 21:22:12 +0100

    Icon prop and problem with typescript

--- src/components/CardServices.tsx
+++ src/components/CardServices.tsx
@@ -1,33 +1,20 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-
-import {
-  IconLookup,
-  IconDefinition,
-  findIconDefinition,
-  IconName
-} from '@fortawesome/fontawesome-svg-core'
-
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 
 interface props {
     textCard: string,
     titleCard: string,
-    source: IconName,
+    source: IconDefinition,
 }
-
 
 export const CardServices = ({ textCard, titleCard, source}: props) => {
   const { t, i18n } = useTranslation();
-  
-  const Icon: IconLookup = { prefix: 'fas', iconName: source };
-  const coffeeIconDefinition: IconDefinition = findIconDefinition(Icon);
-  
   return (
     <div id="services">
       <div className='card'>
-        <FontAwesomeIcon icon={coffeeIconDefinition} className="card__image fa" />
+      <FontAwesomeIcon icon={source} className="card__image"/>
         <h3 className="card__title">t({titleCard})</h3>
         <p className="card__text">t({textCard})</p>
       </div>
