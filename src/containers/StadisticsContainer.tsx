d34b334ff400c4d4cdb5e65d22f079033e2e8586
Nicolas Cena <nicolasnahuelcena@gmail.com>
2022-11-08 22:58:19 +0100

    [HEADER]: Traducciones del header

--- src/containers/StadisticsContainer.tsx
+++ src/containers/StadisticsContainer.tsx
@@ -1,12 +1,7 @@
-import React from 'react'
-import CountUp from 'react-countup';
-import { useTranslation } from 'react-i18next';
 import { CardServices } from '../components/CardServices';
 import { dataStadistic } from '../Mock/dataServices';
 
 export const StadisticsContainer = () => {
-
-    const { t } = useTranslation()
 
   return (
     <div id="stadistics_section">
