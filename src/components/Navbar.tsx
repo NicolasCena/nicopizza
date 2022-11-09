dd57ebfb4cd4f4c7774053aedc47e5b8088b10ca
Nicolas Cena <nicolasnahuelcena@gmail.com>
2022-11-09 12:35:17 +0100

    [NAVBAR]: Corrección de estilos y responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
+import { Nav, Navbar, Container  } from 'react-bootstrap';
 import flagSpain from '../assets/flagSpain.png';
 import flagItaly from '../assets/flagItaly.png';
 import flagGB from '../assets/flagGB.png';
 import { useTranslation } from 'react-i18next';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPizzaSlice} from '@fortawesome/free-solid-svg-icons';
 
 export const NavBar = () => {
 
@@ -16,15 +15,21 @@ export const NavBar = () => {
   };
 
   return (
-    <Navbar expand="lg">
+    <Navbar expand="sm" bg="dark" variant="dark" collapseOnSelect >
       <Container>
-        <Navbar.Brand href="#home">NicoPizza</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <Nav.Link className='languages'>
+        <Navbar.Brand href="#home">
+          <div className='brand__container'>          
+            <FontAwesomeIcon icon={faPizzaSlice} className="icon__container-image"/>
+            <p>NicoPizza</p>
+          </div>
+          <p>DELICIOUS</p>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav>
+            <Nav.Link href="#home" eventKey="1">Home</Nav.Link>
+            <Nav.Link href="#link" eventKey="2">Link</Nav.Link>
+            <Nav.Link className='languages' eventKey="3">
               <img src={flagSpain} alt="" onClick={() => changeLanguage('es')}/>
               <img src={flagItaly} alt="" onClick={() => changeLanguage('it')}/>
               <img src={flagGB} alt="" onClick={() => changeLanguage('en')}/>
